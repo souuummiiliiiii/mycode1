63ff64f03d246ed9467fb6b33484e11422bcea27
souuummiiliiiii <112575662+souuummiiliiiii@users.noreply.github.com>
2022-11-12 00:38:55 +0530

    Add files via upload

---
+++ ascii value of given character.cpp
@@ -0,0 +1,10 @@
+// Print the ascii value of a given character
+#include<stdio.h>
+int main()
+{
+	char a;
+	printf("Enter the character: ");
+	scanf("%c",&a);
+	printf("Ascii value of given a is %d",a);
+	return 0;
+}
